f81249ebe87e38d03e6a375facb721666c641cb1
Mikan95 <andrewmeechan@hotmail.com>
2023-10-16 22:06:19 +0200

    fixed ft_toupper

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,21 +6,22 @@
 /*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 21:43:24 by ameechan          #+#    #+#             */
-/*   Updated: 2023/10/16 22:02:24 by ameechan         ###   ########.fr       */
+/*   Updated: 2023/10/16 22:05:53 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 int ft_toupper(unsigned char c)
 {
-    if (c >= "a" && c <= "z")
+    if (c >= 'a' && c <= 'z')
         c = c - 32;
     return (c);
 }
-
+/* 
 #include <stdio.h>
 #include <ctype.h>
 int main(int argc, char **argv)
 {
-    printf("ft_toupper: %c", ft_toupper(argv[1][0]));
-    printf("   toupper: %c", toupper(argv[1][0]));
-}
+    (void)argc;
+    printf("ft_toupper: %c\n", ft_toupper(argv[1][0]));
+    printf("   toupper: %c\n", toupper(argv[1][0]));
+} */
