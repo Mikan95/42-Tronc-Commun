9a8aad61f44b05eca4762fa0589d2d2e395c5159
Mikan95 <andrewmeechan@hotmail.com>
2023-10-17 01:38:40 +0200

    ft_strrchr now works properly

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,31 +6,35 @@
 /*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 22:44:36 by ameechan          #+#    #+#             */
-/*   Updated: 2023/10/16 22:50:24 by ameechan         ###   ########.fr       */
+/*   Updated: 2023/10/17 01:37:59 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
+
+unsigned long	ft_strlen(char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
 
 char *ft_strrchr(char *str, int c)
 {
     int i;
-    char *ptr;
 
-    i = 0;
-    while(str[i] != '\0')
+    i = ft_strlen(str);
+    while(i >= 0)
     {
         if (str[i] == c)
-        {
-            ptr = &str[i];
-        }
+        return (&str[i]);
         else
-        i++;
+        i--;
     }
-    if (ptr == str)
-    return(ptr);
-    else
     return ('\0');
 }
-
+/* 
 #include <stdio.h>
 #include <bsd/string.h>
 
@@ -39,4 +43,4 @@ int main(int argc, char **argv)
     (void)argc;
     printf("ft_strrchr: %s\n", ft_strrchr(argv[1], argv[2][0]));
     printf("   strrchr: %s\n", strrchr(argv[1], argv[2][0]));
-}
+} */
