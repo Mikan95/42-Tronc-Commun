1edc2744401a15c4c93b8b15301e9006a4f880c3
Mikan95 <andrewmeechan@hotmail.com>
2023-10-18 19:04:22 +0200

    added ft_strnstr, it works and respects the norm

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 18:15:55 by ameechan          #+#    #+#             */
+/*   Updated: 2023/10/18 19:03:25 by ameechan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strnstr(const char *haystack, const char *needle, int len)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (needle[i] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0' && i < len)
+	{
+		j = 0;
+		if (haystack[i] == needle[j])
+		{
+			while (haystack[i + j] == needle[j] && needle[j] != '\0')
+				j++;
+			if (haystack[i + j] == needle[j] || needle[j] == '\0')
+				return ((char *)&haystack[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("Haystack: %s\nNeedle: %s\n", argv[1], argv[2]);
+	printf("ft_strnstr: %s\n", ft_strnstr(argv[1], argv[2], atoi(argv[3])));
+	printf("   strnstr: %s\n", strnstr(argv[1], argv[2], atoi(argv[3])));
+}
+*/
