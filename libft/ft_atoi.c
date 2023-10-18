15cb48d91d30693ec0b9ee091e71c394c3140540
Mikan95 <andrewmeechan@hotmail.com>
2023-10-18 19:13:16 +0200

    added ft_atoi.c it works and respects the norm

---
+++ libft/ft_atoi.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 19:06:16 by ameechan          #+#    #+#             */
+/*   Updated: 2023/10/18 19:12:37 by ameechan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	is_space(char c)
+{
+	if (c == ' ' || c == '\t' || c == '\n')
+		return (1);
+	if (c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	return (0);
+}
+
+int	is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while (is_space(str[i]))
+			i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while (is_digit(str[i]))
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * sign);
+}
+/*
+#include <stdio.h>
+#include <stdlib.h>
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	const char	*str = argv[1];
+	printf("ft_atoi: %d\n", ft_atoi(str));
+	printf("   atoi: %d\n", atoi(str));
+}
+*/
