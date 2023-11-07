48a42a82ede76d2c2aafa5de15fd3be3fc55b81d
Mikan95 <andrewmeechan@hotmail.com>
2023-11-07 15:51:33 +0100

    added strmapi, respects Norm and functions.

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 15:58:21 by ameechan          #+#    #+#             */
-/*   Updated: 2023/11/02 13:25:38 by ameechan         ###   ########.fr       */
+/*   Updated: 2023/11/07 15:50:04 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 #endif
