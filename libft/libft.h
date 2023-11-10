18c55d31f84e787e82dc7517a87762812ed7df1b
Mikan95 <andrewmeechan@hotmail.com>
2023-11-10 14:47:13 +0100

    added lstlast

--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ameechan <ameechan@student.42.ch>           +#+  +:+       +#+       */
+/*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 12:57:21 by ameechan          #+#    #+#             */
-/*   Updated: 2023/11/10 13:06:48 by ameechan         ###   ########.ch       */
+/*   Updated: 2023/11/10 13:17:36 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,4 +56,5 @@ typedef struct s_list
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 #endif
