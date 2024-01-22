22209db622da9151b14f936119785cdaa5328bcc
Mikan95 <andrewmeechan@hotmail.com>
2024-01-22 17:40:38 +0100

    Start of GNL journey

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/22 17:28:46 by ameechan          #+#    #+#             */
+/*   Updated: 2024/01/22 17:34:51 by ameechan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+#endif
