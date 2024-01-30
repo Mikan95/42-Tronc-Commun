52bf26d6c5eed0f4ceca03ccb9216c85f4203c49
Mikan95 <andrewmeechan@hotmail.com>
2024-01-30 23:43:59 +0100

    finished step 3, take what buffer output by read() and store it in linked list.

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 17:32:53 by ameechan          #+#    #+#             */
-/*   Updated: 2024/01/24 19:38:34 by ameechan         ###   ########.fr       */
+/*   Updated: 2024/01/30 23:41:19 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,37 @@
 
 char	*get_next_line(int fd)
 {
-	void	*buffer;
+	void			*buffer;
+	static t_list	*line_stash;
+	int				i;
 
+	line_stash = NULL;
 	buffer = (void *)malloc(BUFFER_SIZE);
-	read(fd, buffer, BUFFER_SIZE);
+	while (read(fd, buffer, BUFFER_SIZE) > 0)
+			line_stash = *fill_stash(buffer, &line_stash);
 	return (buffer);
+}
+
+t_list	**fill_stash(char *buffer, t_list **line_stash)
+{
+	t_list	*current;
+	t_list	*new_node;
+
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->stash = buffer;
+	new_node->next = NULL;
+	if (*line_stash == NULL)
+		*line_stash = new_node;
+	else
+	{
+		current = *line_stash;
+		while (current->next)
+			current = current->next;
+		current->next = new_node;
+	}
+	return (line_stash);
 }
 
 #include <fcntl.h>
@@ -32,5 +58,33 @@ int	main(void)
 		printf("Error opening file");
 		return (1);
 	}
-	printf("read returned:\n%s", get_next_line(fd));
+	get_next_line(fd);
 }
+
+/* char *copy_list_data(t_list *head)
+{
+	size_t total_length;
+	t_list *current;
+
+	total_length = 0;
+	current = head;
+	while (current)
+	{
+		total_length += strlen(current->stash);
+		current = current->next;
+	}
+	char *concatenated_str = (char *)malloc(total_length + 1);
+	if (!concatenated_str)
+		return (NULL);
+	char *ptr = concatenated_str;
+	current = head;
+	while (current)
+	{
+		strcpy(ptr, current->stash);
+		ptr += strlen(current->stash);
+		current = current->next;
+	}
+	*ptr = '\0';
+	return (concatenated_str);
+} */
+
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,21 @@
 /*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 17:28:46 by ameechan          #+#    #+#             */
-/*   Updated: 2024/01/24 19:34:37 by ameechan         ###   ########.fr       */
+/*   Updated: 2024/01/30 23:41:25 by ameechan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 
+typedef struct s_list
+{
+	void					*stash;
+	struct	s_list	*next;
+}							t_list;
 char	*get_next_line(int fd);
+t_list	**fill_stash(char *buffer, t_list **line_stash);
 #endif
