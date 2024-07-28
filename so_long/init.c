09b2df19502483844839522a1c8b8d3558914061
Mikan95 <andrewmeechan@hotmail.com>
2024-07-28 14:20:50 +0200

    made key_hook for ESC_KEY and DestroyNotify work to close window, added background too

--- so_long/init.c
+++ so_long/init.c
@@ -11,16 +11,43 @@
 /* ************************************************************************** */
 
 #include <mlx.h>
+#include "so_long.h"
+
+int	close_window(t_data *data)
+{
+	if (data->win)
+		mlx_destroy_window(data->mlx, data->win);
+	if (data->mlx)
+		free(data->mlx);
+	exit(0);
+}
+
+int	key_hook(int keycode, t_data *data)
+{
+//	printf("keycode: %d\n", keycode);
+	if (keycode == ESC_KEY)
+		close_window(data);
+	return (0);
+}
 
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_data	variables;
+	char	*path = "./assets/Background.xpm";
+	int		img_width;
+	int		img_height;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1024, 768, "Hello world!");
-	(void)mlx_win;
-	mlx_loop(mlx);
+	variables.mlx = mlx_init();
+	if (!variables.mlx)
+		return (1);
+	variables.win = mlx_new_window(variables.mlx, 1024, 768, "Hello world!");
+	if (!variables.win)
+		return (free(variables.mlx), 1);
+	mlx_hook(variables.win, 17, 0, close_window, &variables);
+	mlx_key_hook(variables.win, key_hook, &variables);
+	variables.img_ptr = mlx_xpm_file_to_image(variables.mlx, path, &img_width, &img_height);
+	mlx_put_image_to_window(variables.mlx, variables.win, variables.img_ptr, 0, 0);
+	mlx_loop(variables.mlx);
 }
 
 //gcc init.c -o init -Iminilibx-linux/ -Lminilibx-linux/ -lmlx -lX11 -lXext
---
+++ so_long/so_long.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ameechan <ameechan@student.42.ch>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 12:45:14 by ameechan          #+#    #+#             */
+/*   Updated: 2024/07/28 12:45:14 by ameechan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+#include <mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct s_data
+{
+	void	*mlx;
+	void	*win;
+	void	*img_ptr;
+}				t_data;
+
+//DEFINE KEYCODES
+# define ESC_KEY 65307
+# define W_KEY 119
+# define A_KEY 97
+# define S_KEY 115
+# define D_KEY 100
+#endif
